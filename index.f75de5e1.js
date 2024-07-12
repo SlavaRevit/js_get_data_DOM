978f14a60f74ddf6ec3a58c87dd3665389eb95f2
Slava Filimonenko <=>
2024-07-12 20:19:35 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const averagePopulation = document.querySelector(".average-population");
+const totalPopulation = document.querySelector(".total-population");
+const populationElements = document.querySelectorAll(".population");
+const populations = [
+    ...populationElements
+];
+const populationsInNumbers = populations.map((country)=>{
+    const textNumber = country.innerHTML.replaceAll(",", "");
+    return Number(textNumber);
+});
+const total = populationsInNumbers.reduce((acc, number)=>acc + number, 0);
+const average = total / populations.length;
+totalPopulation.innerHTML = total.toLocaleString("en-US");
+averagePopulation.innerHTML = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
